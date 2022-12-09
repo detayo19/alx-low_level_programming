2c71e47a7ddc0a49d6d946ff3439fb0ec2b53cde
detayo <117822264+detayo19@users.noreply.github.com>
2022-12-09 02:21:16 +0100

    po

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  * Return: Always 0 (Success)
  */
@@ -7,4 +7,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
+}                                              
