175625f4fa7bc6450af9896bfc1ef98d172550dc
detayo <117822264+detayo19@users.noreply.github.com>
2022-12-12 19:04:24 +0100

    sw

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,13 +22,13 @@ if (n > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, nr);
 }
-else if (n == 0)
+else if (n < 6 && n != 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, nr);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nr);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nr);
+printf("Last digit of %d is %d and is 0\n", n, nr);
 }
 return (0);
 }
